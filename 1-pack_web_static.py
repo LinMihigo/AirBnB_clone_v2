5ea4f758ea5620fe490f7e26e6a4a204288e2e51
LinMihigo <linmihigo@gmail.com>
2025-01-25 03:05:08 +0200

    Task 1 (1st fix)

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -2,17 +2,21 @@
 """Contains function do_pack"""
 from fabric.api import local
 from datetime import datetime
-import tarfile
+import os.path
 
 
 def do_pack():
     """Generates a .tgz from contents of web_static"""
-    time = datetime.now().strftime("%Y%m%d%H%M%S")
-    archive = f"web_static_{time}.tgz"
-    print(f"Packing web_static to {archive}")
-    if local(f"tar -cvzf {archive} web_static/", capture=True).failed:
+    dt = datetime.utcnow()
+    file = "versions/web_static_{}{}{}{}{}{}.tgz".format(dt.year,
+                                                         dt.month,
+                                                         dt.day,
+                                                         dt.hour,
+                                                         dt.minute,
+                                                         dt.second)
+    if os.path.isdir("versions") is False:
+        if local("mkdir -p versions").failed is True:
+            return None
+    if local("tar -cvzf {} web_static".format(file)).failed is True:
         return None
-    with tarfile.open(archive, "r:gz") as tar:
-        for member in tar.getmembers():
-            print(member.name)
-    return archive
+    return file

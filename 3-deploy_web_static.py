cb5b149b19fd077e294b7da128f192a13ff8dde2
LinMihigo <linmihigo@gmail.com>
2025-01-25 03:30:16 +0200

    Task 3 (formatting fix)

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -48,7 +48,7 @@ def do_deploy(archive_path):
     if put(archive_path, "/tmp/{}".format(file)).failed is True:
         return False
     if sudo("rm -rf /data/web_static/releases/{}/".
-           format(name)).failed is True:
+            format(name)).failed is True:
         return False
     if run("mkdir -p /data/web_static/releases/{}/".
            format(name)).failed is True:

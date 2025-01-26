dee647b6c1a8ecd286e7a001cf33a3fd64a4e10b
LinMihigo <linmihigo@gmail.com>
2025-01-26 12:44:56 +0200

    Task 0 (nth + 1) fix

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -7,38 +7,40 @@ mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
 
 echo '<html>
-  <head>
-  </head>
-  <body>
+	<head>
+	</head>
+	<body>
 ALX
-  </body>
-</html>' > /data/web_static/releases/test/index.html
+	</body>
+</html>' | tee /data/web_static/releases/test/index.html >/dev/null
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 
-chown -R ubuntu /data
-chgrp -R ubuntu /data/
+chown -R ubuntu:ubuntu /data
 
-printf %s "server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-    add_header X-Served-By $HOSTNAME;
-    root   /var/www/html;
-    index  index.html index.htm;
+CONFIG=$(cat <<EOF
+server {
+	listen 80 default_server;
+	listen [::]:80 default_server;
 
-    location /hbnb_static {
-        alias /data/web_static/current;
-        index index.html index.htm;
-    }
+	add_header X-Served-By $HOSTNAME;
 
-    location /redirect_me {
-        return 301 https://linmihigo.github.io;
-    }
+	root /var/www/html;
+	index index.html index.htm;
 
-    error_page 404 /404.html;
-    location /404 {
-      root /var/www/html;
-      internal;
-    }
-}" > /etc/nginx/sites-available/default
+	error_page 404 /404.html;
 
-service nginx restart
+	location /hbnb_static/ {
+		alias /data/web_static/current/;
+		index index.html index.htm;
+	}
+	location = /404.html {
+		internal;
+	}
+}
+EOF
+)
+
+echo "$CONFIG" | tee /etc/nginx/sites-available/default >/dev/null
+
+nginx -t && service nginx restart
+exit 0
